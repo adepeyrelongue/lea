c22c05cb9fbad3901e70cc5acf65d2155729f86c
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2019-06-13 17:20:03 +0200

    Add models definition.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.name
+
+class SourceSite(models.Model):
+      url = models.CharField(max_length=1000,default="")
+      category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
+
+      def __str__(self):
+         return self.url
+
+class VisitedPage(models.Model):
+      url = models.CharField(max_length=1000,default="")
+      category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
+      source_site = models.ForeignKey(SourceSite, on_delete=models.SET_NULL, blank = True, null = True)
+
+      def __str__(self):
+          return self.url
+
+class Product(models.Model):
+      name =  models.CharField(max_length=500,default="")
+      resume = models.TextField(default="")
+      rating = models.DecimalField(max_digits=3, decimal_places=1)
+      url = models.CharField(max_length=1000,default="")
+      top_sales = models.BooleanField(default=False)
+      top_three_sales = models.BooleanField(default=False)
+      category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
+
+      def __str__(self):
+          return self.url
+
+class Post(models.Model):
+    title = models.CharField(max_length=200,default="")
+    slug = models.SlugField(unique=True, default="")
+    image_src = models.CharField(max_length=500,default="")
+    image_alt = models.CharField(max_length=500,default="")
+    pitch = models.TextField(default="")
+    body = RichTextField(default="")
+    created = models.DateField(default=datetime.now)
+    publish = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
+    visited_page = models.ForeignKey(VisitedPage, on_delete=models.SET_NULL, blank = True, null = True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank = True, null = True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, "home.htm",{'post_list' : post_list})
