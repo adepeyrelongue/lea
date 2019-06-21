336b1c65646b52bec30bde78602b0698ffeecfe8
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2019-06-21 17:31:46 +0200

    Change product model.

--- blog/models.py
+++ blog/models.py
@@ -26,16 +26,25 @@ class VisitedPage(models.Model):
           return self.url
 
 class Product(models.Model):
-      name =  models.CharField(max_length=500,default="")
-      image_src =  models.CharField(max_length=500,default="")
+      full_name =  models.CharField(max_length=500,default="")
+      short_name =  models.CharField(max_length=500,default="")
+      image_src = models.CharField(max_length=500,default="")
       resume = models.TextField(default="")
+      pros = models.TextField(default="")
+      cons = models.TextField(default="")
       rating = models.DecimalField(max_digits=3, decimal_places=1)
       url = models.CharField(max_length=1000,default="")
+      speed_max = models.DecimalField(max_digits=5, decimal_places=1,default=0)
+      autonomy = models.DecimalField(max_digits=5, decimal_places=1,default=0)
+      user_weight_max = models.DecimalField(max_digits=5, decimal_places=1,default=0)
+      composition = models.DecimalField(max_digits=5, decimal_places=1,default=0)
+      dimensions = models.DecimalField(max_digits=5, decimal_places=1,default=0)
+      weight = models.DecimalField(max_digits=5, decimal_places=1,default=0)
       top_sales = models.BooleanField(default=False)
       category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
 
       def __str__(self):
-          return self.name
+          return self.full_name
 
 class Post(models.Model):
     title = models.CharField(max_length=200,default="")
@@ -52,7 +61,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={"slug" : self.slug})
 
