30041bbd7d954f5d0e1ddcf07fd816c42a62a998
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2019-06-25 10:35:28 +0200

    Add product pitch.

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,5 @@ from . import models
 
 admin.site.register(models.Post)
 admin.site.register(models.Category)
-admin.site.register(models.SourceSite)
-admin.site.register(models.VisitedPage)
 admin.site.register(models.Product)
 admin.site.register(models.FirstPage)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from datetime import datetime
 from django.urls import reverse
+import math
 
 
 class Category(models.Model):
@@ -10,41 +11,47 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class SourceSite(models.Model):
-      url = models.CharField(max_length=1000,default="")
-      category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
-
-      def __str__(self):
-         return self.url
-
-class VisitedPage(models.Model):
-      url = models.CharField(max_length=1000,default="")
-      category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
-      source_site = models.ForeignKey(SourceSite, on_delete=models.SET_NULL, blank = True, null = True)
-
-      def __str__(self):
-          return self.url
-
 class Product(models.Model):
       full_name =  models.CharField(max_length=500,default="")
       short_name =  models.CharField(max_length=500,default="")
       image_src = models.CharField(max_length=500,default="")
       resume = models.TextField(default="")
-      pros = models.TextField(default="")
-      cons = models.TextField(default="")
+      pros = RichTextField(default="")
+      cons = RichTextField(default="")
       rating = models.DecimalField(max_digits=3, decimal_places=1)
       url = models.CharField(max_length=1000,default="")
       speed_max = models.DecimalField(max_digits=5, decimal_places=1,default=0)
       autonomy = models.DecimalField(max_digits=5, decimal_places=1,default=0)
       user_weight_max = models.DecimalField(max_digits=5, decimal_places=1,default=0)
-      composition = models.DecimalField(max_digits=5, decimal_places=1,default=0)
-      dimensions = models.DecimalField(max_digits=5, decimal_places=1,default=0)
+      composition = models.CharField(max_length=500,default="")
+      dimensions = models.CharField(max_length=500,default="")
       weight = models.DecimalField(max_digits=5, decimal_places=1,default=0)
       top_sales = models.BooleanField(default=False)
       category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
+      price = models.DecimalField(max_digits=7, decimal_places=2,default=0)
 
       def __str__(self):
-          return self.full_name
+          return self.short_name
+      
+      def get_stars(self):
+          full_stars = math.floor(self.rating)
+          i = 1
+          str = ""
+          limit = full_stars
+
+          while i <= limit:
+            str+="<span class=\"fa fa-star checked\"></span>"
+            i+=1
+          
+          if self.rating - full_stars > 0:
+              str+="<span class=\"fa fa-star-half checked\"></span>"
+              i += 1
+
+          while i <= 5:
+            str+="<span class=\"fa fa-star\"></span>"
+            i+=1
+
+          return str
 
 class Post(models.Model):
     title = models.CharField(max_length=200,default="")
@@ -56,7 +63,6 @@ class Post(models.Model):
     created = models.DateField(default=datetime.now)
     publish = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank = True, null = True)
-    visited_page = models.ForeignKey(VisitedPage, on_delete=models.SET_NULL, blank = True, null = True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank = True, null = True)
 
     def __str__(self):
